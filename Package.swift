89e33efe20ef05b8fd46677f1256adc0385e32da
Harshad <h.v.d@me.com>
2022-06-15 13:23:22 +0200

    wip

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "vwo-ios",
+    products: [
+        .library(
+            name: "vwo-ios",
+            targets: ["vwo-ios"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/socketio/socket.io-client-swift", "15.2.0"..<"15.2.1")
+    ],
+    targets: [
+        .target(
+            name: "vwo-ios",
+            dependencies: [
+                .product(name: "SocketIO", package: "socket.io-client-swift")
+            ],
+            path: "VWO/",
+            publicHeadersPath: "VWO/VWO.h",
+            cSettings: [
+                .headerSearchPath("VWO/")
+            ]
+        )
+    ]
+)
